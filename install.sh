fb915f59881222aaae0b91bd63ea3ab374e9980a
darkusphantom <luisfrodriguezb.lf@gmail.com>
2023-12-27 23:54:01 -0400

    feat: install node and plug install in install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,30 @@ ln -s $HOME/.config/nvim/.vim $HOME/.vim
 ln -s $HOME/.config/nvim/.vimrc $HOME/.vimrc
 sleep 2
 
+echo -e "Verify config..."
+# Verificar si nvm está instalado
+if ! command -v nvm &> /dev/null
+then
+    echo "nvm has not been installed. Installing..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+    source ~/.bashrc
+else
+    echo "nvm has already been installed."
+fi
+sleep 2
+
+# Verificar si Node 15.90 está instalado
+if ! nvm ls | grep '15.90' &> /dev/null
+then
+    echo "Node 15.90 has not been installed. Installing..."
+    nvm install 15.90
+else
+    echo "Node 15.90 has already been installed"
+fi
+sleep 2
+
 echo -e "\nIt's ready! Have a nice day"
 echo -e "\nRemember use the command :PlugInstall when NeoVim is open"
 echo -e "Opening NeoVim..."
 sleep 3
-nvim
+nvim +PlugInstall
